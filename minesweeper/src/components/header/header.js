51fb7e1ff67779622c00cb7affc74a5077c9f64b
Duxcoder <iinegve_25@mail.ru>
2023-05-15 21:28:34 +0300

    feat: add header

--- minesweeper/src/components/header/header.js
+++ minesweeper/src/components/header/header.js
@@ -1,7 +1,16 @@
 import './style.scss';
 
 function header() {
-
+  const $body = document.body;
+  const $header = document.createElement('header');
+  $header.classList.add('header');
+  $header.innerHTML = `
+  <h1 class="logo">
+  <span class='orange'>M</span>INESWEEPER
+</h1>
+  `;
+  $body.prepend($header);
+  return $header;
 }
 
 export default header;
