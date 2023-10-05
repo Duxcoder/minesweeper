fb93c96ca70abba6a2d2326bb0178d6396e91333
Duxcoder <iinegve_25@mail.ru>
2023-10-05 18:29:03 +0300

    feat: create nodeCreator utils function

---
+++ minesweeper/src/utils/utils.js
@@ -0,0 +1,6 @@
+export default function nodeCreator(node, classNames, content) {
+  const $node = document.createElement(node);
+  $node.classList.add(classNames);
+  if (content) $node.innerHTML = content;
+  return $node;
+}
