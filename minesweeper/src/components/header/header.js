f85b6d9e67c564e27f2a4c5fdbaa8ee2f2119b13
Duxcoder <iinegve_25@mail.ru>
2023-10-05 18:29:22 +0300

    refactor: header component

--- minesweeper/src/components/header/header.js
+++ minesweeper/src/components/header/header.js
@@ -1,14 +1,14 @@
 import './style.scss';
+import nodeCreator from '../../utils/utils';
 
 function header() {
   const $body = document.body;
-  const $header = document.createElement('header');
-  $header.classList.add('header');
-  $header.innerHTML = `
+  const headerContent = `
   <h1 class="logo">
   <span class='orange'>M</span>INESWEEPER
 </h1>
   `;
+  const $header = nodeCreator('header', ['header'], headerContent);
   $body.prepend($header);
   return $header;
 }
